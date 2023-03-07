48eb1c6a7024eb8bb580bc7fa60a7ecf860796ca
wummysoft <kaseem.wummy@gmail.com>
2023-03-07 02:16:26 -0800

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,9 +5,9 @@
 #include <stdarg.h>
 
 /**
- * check_cycle -  function in C that checks if a singly linked list has a cycle in it
- * 
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * check_cycle - function in C that checks if a singly linked list has a
+ *	cycle in it
+ *  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
