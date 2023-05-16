c648e19f93b43691c5971eb54c51a495b5ef1a5d
wummysoft <kaseem.wummy@gmail.com>
2023-05-16 15:23:37 -0700

    SQL - Introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Creates a table called first_table in the current database.
-	CREATE TABLE IF NOT EXIST 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXIST 'first_table' ('id' INT, 'name' VARCHAR(256));
